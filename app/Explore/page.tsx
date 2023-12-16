55e163b44ba848daff2bc1fd16dc175772ef592d
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-16 15:36:49 +0530

    Adding the start now rive

--- app/Explore/page.tsx
+++
@@ -1,9 +0,0 @@
-import RiveHero from "@/components/RiveHero";
-
-export default function Home() {
-  return (
-    <main className="block relative w-screen h-screen">
-      <RiveHero />
-    </main>
-  );
-}
