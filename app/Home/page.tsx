3eb39c0577b7b8a30bbdcb4c74ea7bae69cc219a
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-16 20:12:23 +0530

    Adding images in the home page

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import styles from "@/styles/home.module.css"
+import { cn } from "@/lib/utils"
 
 export default function Home() {
   return (
@@ -16,19 +17,38 @@ export default function Home() {
                 <div><a href="https://kanugurajesh.github.io" className={styles.navlist}>Blog</a></div>
             </div>
         </div>
-        <div className={styles.herosection}>
-            <div className={styles.herocontent}>
-                <div className={styles.heroheading}><h1 className={styles.h1}>What is GraphifyEd</h1></div>
-                    <div className={styles.wrapper}>
-                        <p className={`${styles.heroheading} ${styles.p}`}>
-                            GraphifyEd is a unique student learning management system which aims at making learning as addictive as gaming. Start your journey with GraphifyEd and earn points, badges and rewards as you learn.
-                        </p>
-                        <a href="" target="_blank">
-                            <button className={styles.downloadbutton}>Start Learning</button>
-                        </a>
+            <div className={styles.herosection}>
+                <div className={styles.herocontent}>
+                    <div className={styles.heroheading}><h1 className={styles.h1}>What is GraphifyEd</h1>
                     </div>
+                        <div className={styles.wrapper}>
+                            <p className={`${styles.heroheading} ${styles.p}`}>
+                                GraphifyEd is a unique student learning management system which aims at making learning as addictive as gaming. Start your journey with GraphifyEd and earn points, badges and rewards as you learn.
+                            </p>
+                            <a href="" target="_blank">
+                                <button className={styles.downloadbutton}>Start Learning</button>
+                            </a>
+                        </div>
+                </div>
+                <div className={cn(`flex flex-wrap w-[500px] gap-8 ${styles.heroimg}`)}>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                    <Image src="https://codehelp-portfolio-website.netlify.app/images/stack/Javascript.svg" width={90} height={90} alt="hero" className="transition ease-in-out duration-500 hover:scale-110"/>
+                </div>
                 </div>
         </div>
-    </div>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['codehelp-portfolio-website.netlify.app']
+    }
+}
 
 module.exports = nextConfig
