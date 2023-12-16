c44eed5b37cebe9f8d657437402be88273d66e18
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-17 02:36:13 +0530

    Adding mcqs

--- app/Courses/[name]/page.tsx
+++ app/Courses/[name]/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { useEffect, useState } from 'react';
+import { Input } from "@/components/ui/input"
+import { Progress } from "@/components/ui/progress"
+import { cn } from '@/lib/utils';
+import React from 'react'
 
 export default function Page({ params }: { params: { name: string } }) {
+    
     const name = params.name;
     const [questions, setQuestions] = useState([{}]);
+    const [question, setQuestion] = useState();
+    const [progress, setProgress] = useState(10);
 
     // read the file from the file system with the `name`
     const readFile = async (name: string) => {
@@ -17,6 +26,7 @@ export default function Page({ params }: { params: { name: string } }) {
             try {
                 const content = await readFile(name);
                 setQuestions(content?.questions);
+                setQuestion(content?.questions[0]);
             } catch (error) {
                 console.error("Error reading file:", error);
             }
@@ -25,9 +35,45 @@ export default function Page({ params }: { params: { name: string } }) {
     }, [name]);
 
     return (
-        <div>
-            {/* @ts-ignore */}
-            <pre>{JSON.stringify(questions[9]?.question, null, 2)}</pre>
-        </div>
+        <>
+            <div className='flex flex-col mt-5 items-center h-screen gap-10'>
+                {/* @ts-ignore */}
+                {/* <pre>{question?.options[]}</pre> */}
+                <Progress value={progress} className={cn("w-[60%]")} />
+                <div className='w-[60%] flex justify-center'>
+                    {/* @ts-ignore */}
+                    <h1 className='text-2xl font-bold'>{question?.question}</h1>
+                </div>
+                <RadioGroup defaultValue="comfortable">
+                    <div className="flex items-center space-x-2">
+                        {/* @ts-ignore */}
+                        <RadioGroupItem value={question?.options[0]} id="r1" />
+                        {/* @ts-ignore */}
+                        <Label htmlFor="r1">{question?.options[0]}</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        {/* @ts-ignore */}
+                        <RadioGroupItem value={question?.options[1]}id="r2" />
+                        {/* @ts-ignore */}
+                        <Label htmlFor="r2">{question?.options[1]}</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        {/* @ts-ignore */}
+                        <RadioGroupItem value={question?.options[2]}id="r3" />
+                        {/* @ts-ignore */}
+                        <Label htmlFor="r3">{question?.options[2]}</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        {/* @ts-ignore */}
+                        <RadioGroupItem value={question?.options[3]}id="r4" />
+                        {/* @ts-ignore */}
+                        <Label htmlFor="r4">{question?.options[3]}</Label>
+                    </div>
+                </RadioGroup>
+                <button onClick={()=>{
+                    setProgress(progress+10)
+                }}>update</button>
+            </div>
+        </>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.className} overflow-hidden`}>{children}</body>
       </html>
     </ClerkProvider>
   )
