c4a1415f3dcb8c17f0470f3c90ffc0bb9f3278e7
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-16 19:37:13 +0530

    Adding the rive anime

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import RiveHero from "@/components/RiveHero";
 
 export default function Home() {
   return (
-    <main className="block relative w-screen h-screen">
+    <>
       <link
-            rel="preload"
-            href="/hero_use_case_v5.riv"
-            as="fetch"
-            crossOrigin="anonymous"
-          />
-      <RiveHero />
-    </main>
+        rel="preload"
+        href="/hero_use_case_v5.riv"
+        as="fetch"
+        crossOrigin="anonymous"
+      />
+      <main className="block relative w-screen h-screen">
+        <RiveHero />
+      </main>
+    </>
   )
 }
