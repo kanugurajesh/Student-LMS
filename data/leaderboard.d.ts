426463845107d8b736b1467176acd1e410809fbd
kanugurajesh <245122733172@mvsrec.edu.in>
2023-12-17 11:17:46 +0530

    Correcting image

---
+++ data/leaderboard.d.ts
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        "name": "Rajesh",
+        "score": 500
+    },
+    {
+        "name": "Shravan",
+        "score": 400
+    },
+    {
+        "name": "madhur",
+        "score": 300
+    },
+    {
+        "name": "yaswanth",
+        "score": 200
+    },
+]
